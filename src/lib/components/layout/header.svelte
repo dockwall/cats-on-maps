3b46f246a460e44bc623e4faeb27c4b4aafd62b7
Stas <leshuksu@gmail.com>
2023-02-25 20:55:20 +0300

    Added SMUI top app bar and global layout

---
+++ src/lib/components/layout/header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+	let topAppBar: TopAppBar;
+</script>
+
+<TopAppBar bind:this={topAppBar} variant="fixed" dense>
+	<Row>
+		<Section>
+			<IconButton class="material-icons">menu</IconButton>
+			<Title>Fixed</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+			<IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+			<IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+<AutoAdjust {topAppBar} />
+
+<style>
+	/* Hide everything above this component. */
+	:global(#smui-app),
+	:global(body),
+	:global(html) {
+		display: block !important;
+		height: auto !important;
+		width: auto !important;
+		position: static !important;
+	}
+</style>
