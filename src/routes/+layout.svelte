777581c9157168a8eb6646ba4c0bd0822f6a5141
Stanislav Leshuk <leshuksu@gmail.com>
2023-03-07 17:03:56 +0300

    Fix building and deploying (#12)
    * Fixed prerender options and replaced it
    
    * Replaced normalize.css with npm package, Imported it
    
    * Added fallback, toggled prerender option

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	export const prerender = true;
-
 	import { base } from '$app/paths';
+	import 'normalize.css';
 	import Header from '$lib/components/layout/header.svelte';
 </script>
 
