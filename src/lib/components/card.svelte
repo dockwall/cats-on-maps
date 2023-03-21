b03830a25eb45f034bb2a973a5a4f7d22170b067
Stas <leshuksu@gmail.com>
2023-03-21 23:37:57 +0300

    Recreated banner, added gradient

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Card, {
+		Content,
+		PrimaryAction,
+		Media,
+		MediaContent,
+		Actions,
+		ActionButtons,
+		ActionIcons
+	} from '@smui/card';
+	import Button, { Label } from '@smui/button';
+	import IconButton, { Icon } from '@smui/icon-button';
+
+	let clicked = 0;
+</script>
+
+<div class="card-container">
+	<Card>
+		<PrimaryAction on:click={() => clicked++}>
+			<Content class="mdc-typography--body2">
+				<h2 class="card-container__title mdc-typography--headline6">Счётчик записей.</h2>
+				<h3 class="mdc-typography--subtitle2">Скоро он будет здесь!</h3>
+			</Content>
+			<Actions>
+				<ActionButtons>
+					<Button>
+						<Label>Войти</Label>
+					</Button>
+					<Button color="secondary">
+						<Label>Карта</Label>
+					</Button>
+					<Button color="secondary">
+						<Label>Список</Label>
+					</Button>
+				</ActionButtons>
+			</Actions>
+		</PrimaryAction>
+	</Card>
+</div>
+
+<style lang="scss">
+	.card-container {
+		width: fit-content;
+
+		&__title {
+			margin: 0;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
-	import MainPageBanner from '$lib/assets/images/main-page.jpg';
 </script>
 
 <main class="mainpage">
 	<div class="mainpage__banner">
-		<div>
-			<img class="mainpage__banner-img" src={MainPageBanner} alt="Stray cats" />
-		</div>
+		<h1 class="mainpage__banner-title">
+			Неглаженные, грустные, голодные коты ждут тебя во дворах вокруг
+		</h1>
 	</div>
 </main>
 
-<p>This is main + about page</p>
-
 <style lang="scss">
 	.mainpage {
+		background: linear-gradient(180deg, rgba(0, 150, 136, 1) 0%, rgba(255, 193, 7, 1) 100%);
 		&__banner {
-			position: relative;
-			&-img {
-				width: 100%;
+			height: calc(100vh - 64px);
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+
+			&-title {
+				margin: 0;
+				color: white;
 			}
 		}
 	}
