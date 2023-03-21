2fb67ebd8f4debb8db6dfc77715132369f1c6384
Stas <leshuksu@gmail.com>
2023-03-21 21:16:10 +0300

    Created banner with image. Added SMUI card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import MainPageBanner from '$lib/assets/images/main-page.jpg';
+</script>
+
+<main class="mainpage">
+	<div class="mainpage__banner">
+		<div>
+			<img class="mainpage__banner-img" src={MainPageBanner} alt="Stray cats" />
+		</div>
+	</div>
+</main>
 
 <p>This is main + about page</p>
+
+<style lang="scss">
+	.mainpage {
+		&__banner {
+			position: relative;
+			&-img {
+				width: 100%;
+			}
+		}
+	}
+</style>
