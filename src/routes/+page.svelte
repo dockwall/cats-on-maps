87aa0142fffe35ebff8d0cc8f6a58bde5ec7ced8
Stas <leshuksu@gmail.com>
2023-03-23 14:03:49 +0300

    Added MUI Typography, used Roboto

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,12 @@
 
 <main class="mainpage">
 	<div class="mainpage__banner">
-		<h1 class="mainpage__banner-title">
-			Неглаженные, грустные, голодные коты ждут тебя во дворах вокруг
+		<h1 class="mdc-typography--headline1 mainpage__banner-title">
+			Неглаженные, грустные и голодные коты
 		</h1>
+		<h2 class="mdc-typography--headline2 mainpage__banner-subtitle">
+			ждут тебя во дворах вокруг &#128008;
+		</h2>
 	</div>
 </main>
 
@@ -14,14 +17,21 @@
 		background: linear-gradient(180deg, rgba(0, 150, 136, 1) 0%, rgba(255, 193, 7, 1) 100%);
 		&__banner {
 			height: calc(100vh - 64px);
+			box-sizing: border-box;
+			padding-top: 20vh;
 			display: flex;
-			flex-direction: row;
-			justify-content: center;
+			flex-direction: column;
 			align-items: center;
 
 			&-title {
 				margin: 0;
+				font-size: 60px;
 				color: white;
+			}
+
+			&-subtitle {
+				margin: 0;
+				font-size: 45px;
 			}
 		}
 	}
